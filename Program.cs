b9ff8fb627e40b6575463ca5a1b12a10dc624202
ahmed23jd4-star <ahmed23jd4@gmail.com>
2026-09-12 02:53:46 +0300

    advancedc#q1

--- Program.cs
+++ Program.cs
@@ -5,7 +5,13 @@ namespace advancedc_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region q1
+            //Q1: What is a generic class? Why use generics?
+
+            //هو عباره عن اني بخليه بشكل عام واحد نوع داتا تايب لما اجي استخدم الكلاس ده وبعد تحديد داتا اي حاجه كنت مسميها ب اسم جينيريك بتتحول للي كتبته
+            // beteer performance , readsabilty , وعدم تكرار الكود لكل داتا تايب
+            #endregion
+
         }
     }
 }
